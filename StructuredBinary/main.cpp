3bbb0d722f3874fe8d62d0dbefa6c4743ecde1cc
Ronald Pieket <rpieket@sunflower-2.local>
2012-05-13 14:23:56 -0700

    Initial Commit

---
+++ StructuredBinary/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  StructuredBinary
+//
+//  Created by Ronald Pieket-Weeserik on 5/13/12.
+//  Copyright 2012 It Should Just Work!™. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
